4ccfb1be352662d8d47432586155240679a63b9a
TasneemFarid <tasneemfarid.webdev@gmail.com>
2025-02-18 19:04:24 +0600

    certificates page done

--- src/app/pages/certificates/certificates.component.ts
+++ src/app/pages/certificates/certificates.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { HeadingComponent } from '../../components/heading/heading.component';
+import { ButtonsComponent } from '../../components/buttons/buttons.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-certificates',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    HeadingComponent,
+    ButtonsComponent,
+    FooterComponent
+  ],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
