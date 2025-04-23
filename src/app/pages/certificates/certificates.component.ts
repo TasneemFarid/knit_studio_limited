8f697e9d813fe2df5c171aae6b97a86f476718f5
TasneemFarid <tasneemfarid.webdev@gmail.com>
2025-04-23 13:04:52 +0600

    certificates part done

--- src/app/pages/certificates/certificates.component.ts
+++ src/app/pages/certificates/certificates.component.ts
@@ -17,10 +17,45 @@ import { FooterComponent } from '../../components/footer/footer.component';
   styleUrl: './certificates.component.css'
 })
 export class CertificatesComponent {
-  downloadPdf() {
+  oekoTexPdfDownload() {
     const link = document.createElement('a');
-    link.href = 'assets/certificates/NSI - AD APPLICATION.pdf';
-    link.download = 'sample.pdf';
+    link.href = 'assets/certificates/Oekotex Certificate -2024-25.pdf';
+    link.download = 'Oekotex Certificate -2024-25.pdf';
+    link.click();
+  }
+
+  GrsPdfDownload() {
+    const link = document.createElement('a');
+    link.href = 'assets/certificates/GRS Certificate 2024-2025 .pdf';
+    link.download = 'GRS Certificate 2024-2025 .pdf';
+    link.click();
+  }
+  
+  BsciPdfDownload() {
+    const link = document.createElement('a');
+    link.href = 'assets/certificates/BSCI Audit Report -2024-26.pdf';
+    link.download = 'BSCI Audit Report -2024-26.pdf';
+    link.click();
+  }
+
+  SedexPdfDownload() {
+    const link = document.createElement('a');
+    link.href = 'assets/certificates/Sedex Report 2024-25.pdf';
+    link.download = 'Sedex Report 2024-25.pdf';
+    link.click();
+  }
+
+  GotsPdfDownload() {
+    const link = document.createElement('a');
+    link.href = 'assets/certificates/122087_GOTS_Certificate_Knit Studio Limited_20241021_signed.pdf';
+    link.download = '122087_GOTS_Certificate_Knit Studio Limited_20241021_signed.pdf';
+    link.click();
+  }
+
+  IcsPdfDownload() {
+    const link = document.createElement('a');
+    link.href = 'assets/certificates/ICS Audit Report -2024-25.pdf';
+    link.download = 'ICS Audit Report -2024-25.pdf';
     link.click();
   }
 }
