b483076166b3bcd44999863b1c0b5194ed67f3a6
TasneemFarid <tasneemfarid.webdev@gmail.com>
2025-06-13 11:56:00 +0600

    .

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HeadingComponent } from '../../components/heading/heading.component';
 import { ButtonsComponent } from '../../components/buttons/buttons.component';
@@ -18,11 +18,173 @@ import { initFlowbite } from 'flowbite';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+
+  @Output() details = new EventEmitter<any>();
+
   ngOnInit(): void {
     initFlowbite();
   }
 
-  viewDetails(){
-    
-  }
+  productsMens = [
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8931 copy.jpg"
+    },
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8933 copy.jpg"
+    },
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8850 copy.jpg"
+    },
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8928 copy.jpg"
+    },
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8837 copy.jpg"
+    },
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8856 copy.jpg"
+    },
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8871 copy.jpg"
+    },
+    {
+      name: "Men's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/men/195A8898 copy.jpg"
+    },
+  ];
+
+  productsWomens = [
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8809 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8828 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8831 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8861 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8862 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8863 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8864 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8866 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8867 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8889 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8892 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8894 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8897 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8899 copy.jpg"
+    },
+    {
+      name: "Women's T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/women/195A8904 copy.jpg"
+    },
+  ];
+
+  productsKids = [
+    {
+      name: "Kids T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/kids/195A8817 copy.jpg"
+    },
+    {
+      name: "Kids T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/kids/195A8820 copy.jpg"
+    },
+    {
+      name: "Kids T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/kids/195A8821 copy.jpg"
+    },
+    {
+      name: "Kids T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/kids/195A8823 copy.jpg"
+    },
+    {
+      name: "Kids T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/kids/195A8824 copy.jpg"
+    },
+    {
+      name: "Kids T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/kids/195A8830 copy.jpg"
+    },
+    {
+      name: "Kids T-Shirt",
+      material: "Silk, Cashmere, Virgin Wool",
+      image: "../../../assets/products/kids/195A8895 copy.jpg"
+    },
+  ];
+
+  viewDetails(value: any): void {
+  this.details.emit(value);
+}
 }
